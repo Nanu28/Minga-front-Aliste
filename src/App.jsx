3b9bb5863ea457ceec024c1fe2199accd581f5fb
Nanu28 <na.aliste@live.cl>
2023-08-29 19:44:44 -0400

    configuración layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import Layout from './layouts/Layout'
+import Home from './pages/Home.jsx'
+import Layout from './layouts/Layout.jsx'
 
 function App() {
   return (
     <>
       <div className='bg-[url("../public/images/background-mobile-home-naruto.svg")] bg-cover bg-start w-full md:bg-none'>   
-        <Layout/>
+        <Layout>
+          <Home/>
+        </Layout>
       </div>
     </>
   )
